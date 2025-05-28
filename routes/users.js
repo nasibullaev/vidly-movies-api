15c293b42177fe88d6bc7d351a62f25bd9ebcbbd
nasibullaev <fnasibullayev@gmail.com>
2025-05-28 20:29:19 +0500

    isAdmin added

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
+const auth = require("../middleware/auth");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const { User, validate } = require("../models/user");
-const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
-// Get all users
-router.get("/", async (req, res) => {
-  const users = await User.find();
-  res.send(users);
+// get the current user
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.status(200).send(user);
 });
 
-// Create a user
+// create a user
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -27,44 +27,6 @@ router.post("/", async (req, res) => {
   await user.save();
   const token = user.generateAuthToken();
   res.header("x-auth-token", token).send(_.pick(user, ["name", "email"]));
-});
-
-// Edit user details
-router.put("/:id", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const { name, email, password } = req.body;
-
-  const user = await User.findByIdAndUpdate(
-    req.params.id,
-    { name, email, password },
-    { new: true }
-  );
-
-  if (!user) return res.status(404).send("User not found");
-  res.send(_.pick(user, ["name", "email"]));
-});
-
-// Delete a user
-router.delete("/:id", async (req, res) => {
-  const user = await User.findByIdAndDelete(req.params.id);
-  if (!user) return res.status(404).send("User not found with given id");
-  res.send(user);
-});
-
-// Get one specific user
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send("Invalid user ID");
-  }
-
-  const user = await User.findById(id);
-  if (!user) return res.status(404).send("User not found");
-
-  res.send(_.pick(user, ["name", "email"]));
 });
 
 module.exports = router;
